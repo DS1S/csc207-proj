c27764ff384a8b7c6ec782d4ec517c0af041e916
ed963 <64994681+ed963@users.noreply.github.com>
2020-11-03 12:57:36 -0500

    Finished implementation of Entity classes related to Scheduling system.

--- phase1/src/CoreEntities/Event.java
+++ phase1/src/CoreEntities/Event.java
@@ -1,4 +1,69 @@
 package CoreEntities;
 
-public class Event {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.time.LocalTime;
+
+public class Event implements Serializable {
+    private int capacity;
+    private List<User> attendees;
+    private String room;
+    private LocalTime startTime;
+    private String title;
+    private User speaker;
+    private int duration;
+
+    public Event(int capacity, String room, LocalTime startTime, String title, User speaker) {
+        this.capacity = capacity;
+        this.attendees = new ArrayList<>();
+        this.room = room;
+        this.startTime = startTime;
+        this.title = title;
+        this.speaker = speaker;
+        this.duration = 60;
+    }
+
+    public int getCapacity() { return this.capacity; }
+
+    public void setCapacity(int capacity) { this.capacity = capacity; }
+    
+    public boolean atCapacity() { return this.attendees.size() == capacity; }
+
+    public List<User> getAttendees() { return this.attendees; }
+
+    public void addAttendee(User attendee) { this.attendees.add(attendee); }
+
+    public void removeAttendee(User attendee) { this.attendees.remove(attendee); }
+    
+    public boolean checkAttendee(User attendee) { return this.attendees.contains(attendee); }
+
+    public String getRoom() { return this.room; }
+
+    public void setRoom(String room) { this.room = room; }
+
+    public LocalTime getStartTime() { return this.startTime; }
+
+    public void setStartTime(LocalTime time) { this.startTime = time; }
+
+    public LocalTime getEndTime() { return startTime.plusMinutes(duration); }
+
+    public String getTitle() { return this.title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public User getSpeaker() { return this.speaker; }
+
+    public void setSpeaker(User speaker) { this.speaker = speaker; }
+
+    public int getDuration() { return this.duration; }
+
+    public void setDuration(int duration) { this.duration = duration; }
+
+    @Override
+    public String toString() {
+        return '"' + title + "\" by " + speaker + "\n" +
+                startTime + " to " + startTime.plusMinutes(duration) + " in " + room;
+    }
 }
--- phase1/src/CoreEntities/Schedule.java
+++ phase1/src/CoreEntities/Schedule.java
@@ -1,5 +1,127 @@
 package CoreEntities;
 
-public class Schedule {
+import java.time.LocalTime;
+import java.util.*;
 
+public class Schedule implements Iterable<Event> {
+    private List<Event> events;
+
+    public Schedule() {
+        this.events = new ArrayList<>();
+    }
+
+    public Schedule(List<Event> events) { this.events = events; }
+
+    public void addEvent(Event event) {
+        int i = 0;
+        while (events.get(i).getStartTime().isBefore(event.getStartTime())) {
+            i++;
+        }
+        events.add(i, event);
+    }
+
+    public Schedule retrieveEventByTime(LocalTime time) {
+        List<Event> matchedEvents = new ArrayList<>();
+        for (Event event: events) {
+            if ((event.getStartTime().isBefore(time) || event.getStartTime().equals(time)) &&
+                    event.getEndTime().isAfter(time)) {
+                matchedEvents.add(event);
+            }
+        }
+        return new Schedule(matchedEvents);
+    }
+
+    public Schedule retrieveEventBySpeaker(User speaker) {
+        List<Event> matchedEvents = new ArrayList<>();
+        for (Event event: events) {
+            if (event.getSpeaker().equals(speaker)) {
+                matchedEvents.add(event);
+            }
+        }
+        return new Schedule(matchedEvents);
+    }
+
+    public Schedule retrieveEventByTitle(String title) {
+        List<Event> matchedEvents = new ArrayList<>();
+        for (Event event: events) {
+            if (event.getTitle().equals(title)) {
+                matchedEvents.add(event);
+            }
+        }
+        return new Schedule(matchedEvents);
+    }
+
+    public Schedule retrieveEventByAttendee(User attendee) {
+        List<Event> matchedEvents = new ArrayList<>();
+        for (Event event: events) {
+            if (event.checkAttendee(attendee)) {
+                matchedEvents.add(event);
+            }
+        }
+        return new Schedule(matchedEvents);
+    }
+
+    public Schedule retrieveSignupAbleEvents(User attendee) {
+        List<Event> matchedEvents = new ArrayList<>();
+        for (Event event: events) {
+            if (!event.checkAttendee(attendee) && !event.atCapacity()) {
+                matchedEvents.add(event);
+            }
+        }
+        return new Schedule(matchedEvents);
+    }
+
+    public Event retrieveEventByIndex(int index) throws IndexOutOfBoundsException {
+        return this.events.get(index);
+    }
+
+    @Override
+    public String toString() {
+        int i = 0;
+        StringBuilder scheduleStr = new StringBuilder();
+        for (Event event: events) {
+            String newRow = i + ") " + event + "\n\n";
+            scheduleStr.append(newRow);
+        }
+        return scheduleStr.toString();
+    }
+
+    @Override
+    public Iterator<Event> iterator() { return new ScheduleIterator(); }
+
+    private class ScheduleIterator implements Iterator<Event> {
+
+        /** The index of the next Event to return. */
+        private int current = 0;
+
+        /**
+         * Returns whether there is another Event to return.
+         * @return whether there is another Event to return.
+         */
+        @Override
+        public boolean hasNext() {
+            return current < events.size();
+        }
+
+        /**
+         * Returns the next Event.
+         * @return the next Event.
+         */
+        @Override
+        public Event next() {
+            Event res;
+
+            // List.get(i) throws an IndexOutBoundsException if
+            // we call it with i >= events.size().
+            // But Iterator's next() needs to throw a
+            // NoSuchElementException if there are no more elements.
+            try {
+                res = events.get(current);
+            } catch (IndexOutOfBoundsException e) {
+                throw new NoSuchElementException();
+            }
+            current += 1;
+            return res;
+        }
+    }
 }
