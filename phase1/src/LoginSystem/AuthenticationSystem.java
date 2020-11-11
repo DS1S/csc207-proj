2f15cac8816177de1c76b8be56692b3a9726c462
Vedang Naik <vedang.a.naik@gmail.com>
2020-11-12 00:59:44 +0530

    Moved input taking responsibility to AuthenticationSystem, left AuthenticationUI with only presenter duties.

--- phase1/src/LoginSystem/AuthenticationSystem.java
+++ phase1/src/LoginSystem/AuthenticationSystem.java
@@ -2,20 +2,67 @@ package LoginSystem;
 
 import LoginSystem.Exceptions.*;
 
-public class AuthenticationSystem {
+import java.util.Scanner;
+
+public class AuthenticationSystem implements Runnable {
     private LoginHandler loginHandler;
     private SignupHandler signupHandler;
+    private AuthenticationUI authUI;
 
     public AuthenticationSystem(UserManager userManager){
         loginHandler = new LoginHandler(userManager);
         signupHandler = new SignupHandler(userManager);
+        authUI = new AuthenticationUI();
     }
 
-    public void signUp(String username, String password) throws UsernameTakenException, DuplicateUUIDException, InvalidUsertypeException {
-        signupHandler.signUp(username, password, "attendee");
-    }
+    @Override
+    public void run() {
+        this.authUI.displayWelcomePage();
+        Scanner in = new Scanner(System.in);
+        int option = in.nextInt();
+        while (option != 1 && option != 2) {
+            this.authUI.displayOptionError();
+            option = in.nextInt();
+        }
 
-    public void LogIn(String username, String password) throws InvalidPasswordException, InvalidUsernameException {
-        loginHandler.loginUser(username, password);
+        if (option == 1) {
+            this.authUI.displayLoginPage();
+            this.authUI.displayUsernamePrompt();
+            String username = in.nextLine();
+            this.authUI.displayPasswordPrompt();
+            String password = in.nextLine();
+            while (true) {
+                try {
+                    this.loginHandler.loginUser(username, password);
+                    break;
+                } catch (InvalidUsernameException e) {
+                    this.authUI.displayError(e.getMessage());
+                    this.authUI.displayUsernamePrompt();
+                    username = in.nextLine();
+                } catch (InvalidPasswordException e) {
+                    this.authUI.displayError(e.getMessage());
+                    this.authUI.displayPasswordPrompt();
+                    password = in.nextLine();
+                }
+            }
+        } else {
+            this.authUI.displaySigningUpPage();
+            this.authUI.displayUsernamePrompt();
+            String username = in.nextLine();
+            this.authUI.displayPasswordPrompt();
+            String password = in.nextLine();
+            while (true) {
+                try {
+                    this.signupHandler.signUp(username, password, "attendee");
+                    break;
+                } catch (UsernameTakenException e) {
+                    this.authUI.displayError(e.getMessage());
+                    this.authUI.displayUsernamePrompt();
+                    username = in.nextLine();
+                } catch (Exception e) {
+                    this.authUI.displayError(e.getMessage());
+                }
+            }
+        }
     }
 }
