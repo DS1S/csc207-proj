26ecdecefd750a4e04bb79bc184518c60b328283
demadani <daniel.dema@mail.utoronto.ca>
2020-11-11 11:30:28 -0500

    started MessageSystem, exception class for one of the MessageSystem methods

---
+++ phase1/src/MessagingSystem/MessageSystem.java
@@ -0,0 +1,11 @@
+package MessagingSystem;
+
+public class MessageSystem {
+    private MessageManager messageManager;
+
+    public MessageSystem(MessageManager messageManager){
+        this.messageManager = messageManager;
+    }
+
+
+}
