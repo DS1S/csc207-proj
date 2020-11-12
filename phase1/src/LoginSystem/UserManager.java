78909bca57a46469b202e08b29a31f10ed45bc23
demadani <daniel.dema@mail.utoronto.ca>
2020-11-12 03:43:33 -0500

    added method to get username with UUID

--- phase1/src/LoginSystem/UserManager.java
+++ phase1/src/LoginSystem/UserManager.java
@@ -8,29 +8,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-/**
- * A class the manages a list of User objects.
- */
 public class UserManager {
     private Map<UUID, User> users;
     private User loggedInUser;
-
 
     public UserManager(){
         this.users = new HashMap<UUID, User>();
     }
 
-    /**
-     * Adds a user to the list of users managed by UserManager.
-     *
-     * @param type A string which represents the type of this user, used by the UserFactory to make the appropriate user. Is presumed to be valid, checked by caller.
-     * @param username username uniqueness is presumed checked by the caller.
-     * @param password the user's password
-     * @param name the user's real-life name
-     */
     public void addUser(String type, String username, String password, String name) {
         UserFactory userCreator = new UserFactory();
-        User u;
+        User u = null;
 
         do {
             u = userCreator.buildUser(type, username, password, name);
@@ -52,13 +40,7 @@ public class UserManager {
         return false;
     }
 
-    /**
-     * Checks if password matches that of the the user with UUID id.
-     *
-     * @param id is presumed to be unique.
-     * @param password the input password to check against stored.
-     * @return true if password matches stored password, false otherwise
-     */
+    //precon: uuid exists
     public boolean checkPasswordWithUUID(UUID id, String password){
         return users.get(id).checkPassword(password);
     }
@@ -80,13 +62,12 @@ public class UserManager {
         this.loggedInUser = this.users.get(userID);
     }
 
-    /**
-     * Checks if the logged in user has a particular permission.
-     *
-     * @param permission a permission from the Perms enum
-     * @return true if the logged-in user has this permission, false otherwise
-     */
     public boolean loggedInHasPermission(Perms permission){
-        return this.loggedInUser.getPermissions().get(permission);
+        Boolean hasPerm = this.loggedInUser.getPermissions().get(permission);
+        return hasPerm;
+    }
+
+    public String getUsernameWithUUID(UUID userUUID){
+        return users.get(userUUID).getName();
     }
 }
