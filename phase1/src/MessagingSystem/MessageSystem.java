cc58223ad9ed861ce04a4d31d819441cd2765279
demadani <daniel.dema@mail.utoronto.ca>
2020-11-12 00:27:54 -0500

    started MessageAttendees method - will complete soon

--- phase1/src/MessagingSystem/MessageSystem.java
+++ phase1/src/MessagingSystem/MessageSystem.java
@@ -1,14 +1,21 @@
 package MessagingSystem;
+import CoreEntities.Users.Perms;
 import LoginSystem.UserManager;
 import coreUtil.IRunnable;
-
+import CoreEntities.FriendsList;
+import CoreEntities.Users.User;
+import CoreEntities.Event;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.UUID;
 import java.util.List;
+import SchedulingSystem.EventManager;
 
 public class MessageSystem implements IRunnable {
     private MessageManager messageManager;
     private UserManager userManager;
+    private final String NOT_A_FRIEND = "That user is not on your friends list!";
+    private final String DO_NOT_HAVE_PERMISSION = "You do not have permission to do that!";
 
     public MessageSystem(MessageManager messageManager, UserManager userManager){
         this.messageManager = messageManager;
@@ -31,4 +38,17 @@ public class MessageSystem implements IRunnable {
         messageManager.sendMessageToMultiple(userManager.getLoggedInUserUUID(),
                 recipientUUIDs, message);
     }
+
+    public String MessageAttendees(List<String> events, String message) {
+        if (!userManager.loggedInHasPermission(Perms.canMessageTalk)) {
+            return DO_NOT_HAVE_PERMISSION;
+        }
+
+        List<Event> eventList = new ArrayList<>();
+        List<UUID> attendeeUUIDs = new ArrayList<>();
+        return "";
+
+    }
+
+
 }
