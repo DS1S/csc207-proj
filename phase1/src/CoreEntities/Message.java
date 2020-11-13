cf5f6d1b2dc04809c30d9a933ffc1a1e81921e5b
demadani <daniel.dema@mail.utoronto.ca>
2020-11-13 01:34:38 -0500

    added javadocs for Message

--- phase1/src/CoreEntities/Message.java
+++ phase1/src/CoreEntities/Message.java
@@ -7,13 +7,33 @@ import java.lang.String;
 import java.time.LocalTime;
 import java.io.Serializable;
 
+/**
+ * A message with a message id, a sender UUID, a recipient UUID, a body and a timestamp.
+ */
 public class Message implements Serializable {
+    /** The id of the message. */
     private UUID msgId;
+
+    /** The UUID of the sender of the message. */
     private UUID sender;
+
+    /** The UUID of the recipient of the message. */
     private UUID recipient;
+
+    /** The body of the message. */
     private String body;
+
+    /** The time at which the message is sent. */
     private LocalTime timeSent;
 
+    /**
+     * Constructs a new Message with the information below.
+     * @param msgId the message id
+     * @param sender the message sender
+     * @param recipient the message recipient
+     * @param body the message body
+     * @param timeSent the time the message was sent
+     */
     public Message(UUID msgId, UUID sender, UUID recipient, String body, LocalTime timeSent){
         this.msgId = msgId;
         this.sender = sender;
@@ -22,15 +42,28 @@ public class Message implements Serializable {
         this.timeSent = timeSent;
     }
 
+    /**
+     * Returns the UUID of the message sender.
+     * @return message sender UUID
+     */
     public UUID getSender() {
         return sender;
     }
 
+    /**
+     * Returns the UUID of the message recipient.
+     * @return message recipient UUID
+     */
     public UUID getRecipient(){
         return recipient;
     }
 
 
+    /**
+     * Returns a map whose values are the message's properties corresponding to those properties in
+     * string format.
+     * @return a map whose values are message properties
+     */
     public Map<String, Object> extractData() {
         Map<String, Object> messageData = new HashMap<>();
 
