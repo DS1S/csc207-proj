5e5de4e10ab901963a45d4e799518ba0163917d0
demadani <daniel.dema@mail.utoronto.ca>
2020-11-18 13:06:13 -0500

    added javadocs for ErrorUI

--- phase1/src/Presenters/ErrorUI.java
+++ phase1/src/Presenters/ErrorUI.java
@@ -1,6 +1,14 @@
 package Presenters;
 
+/**
+ * A class responsible for errors displayed to the user.
+ */
 public class ErrorUI {
+
+    /**
+     * Displays an error to the user.
+     * @param s error that is displayed
+     */
     public void displayError(String s){
         System.out.println(s);
     }
