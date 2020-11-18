e35003f0bb81e21fd8bc52217a006e1d9a7381a7
Vedang Naik <vedang.a.naik@gmail.com>
2020-11-18 23:29:17 +0530

    Added javadocs to AuthenticationSystem and AuthenticationUI

--- phase1/src/LoginSystem/AuthenticationSystem.java
+++ phase1/src/LoginSystem/AuthenticationSystem.java
@@ -5,12 +5,19 @@ import coreUtil.IRunnable;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * A class to handle logging in of existing users and signing up of new ones.
+ */
 public class AuthenticationSystem implements IRunnable {
     private final AuthenticationUI authUI;
     private final LoginSystem loginSystem;
     private final SignupSystem signupSystem;
     private Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Creates a new AuthenticationSystem to use the input UserManager to do it's duties.
+     * @param userManager the UserManager to work with.
+     */
     public AuthenticationSystem(UserManager userManager){
         authUI = new AuthenticationUI();
         loginSystem = new LoginSystem(userManager);
@@ -19,6 +26,9 @@ public class AuthenticationSystem implements IRunnable {
         signupSystem = new SignupSystem(userManager, ATTENDEE_TYPE);
     }
 
+    /**
+     * The run method implementation required by IRunnable. Handles user input for choosing between login and signup.
+     */
     @Override
     public void run() {
         int option = 0;
@@ -36,6 +46,10 @@ public class AuthenticationSystem implements IRunnable {
         }
     }
 
+    /**
+     * Processes the user's input once it's verified.
+     * @param input the verified input of the user.
+     */
     private void processInput(int input){
         switch (input) {
             case (1):
