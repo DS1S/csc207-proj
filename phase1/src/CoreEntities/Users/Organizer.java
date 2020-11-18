6c32937dd3d71dbdb5290ae32b186919843f5b95
Daniel Saunders <daniel.saunders@mail.utoronto.ca>
2020-11-18 12:35:30 -0500

    JavaDocs for all User objects.

--- phase1/src/CoreEntities/Users/Organizer.java
+++ phase1/src/CoreEntities/Users/Organizer.java
@@ -1,7 +1,16 @@
 package CoreEntities.Users;
 
+/**
+ * Represents an organizer type user.
+ */
 public class Organizer extends User {
 
+    /**
+     * Creates an Organizer Object and set its permissions accordingly based on its type.
+     * @param name Name of the organizer.
+     * @param username Username of the organizer.
+     * @param password Password of the organizer.
+     */
     public Organizer(String name, String username, String password) {
         super(name, username, password);
         setPermissions();
