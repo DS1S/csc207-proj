54147489077dd17f725f5a42dbaadcccf2d0cc94
demadani <daniel.dema@mail.utoronto.ca>
2020-11-18 13:12:30 -0500

    added javadocs for MainMenuUI

--- phase1/src/Presenters/MainMenuUI.java
+++ phase1/src/Presenters/MainMenuUI.java
@@ -4,10 +4,20 @@ import coreUtil.IRunnable;
 
 import java.util.Map;
 
+/**
+ * The class responsible for the main menu displayed to the user.
+ */
 public class MainMenuUI {
 
+    /**
+     * Constructs an instance of MainMenuUI.
+     */
     public MainMenuUI() { }
 
+    /**
+     * Displays main menu options to the user.
+     * @param subsystems the subsystems that implement the actions the users can take
+     */
     public void displayOptions(Map<Integer, IRunnable> subsystems) {
         for (int i = 1; i < subsystems.size(); i++) {
             System.out.println(i + ": " + subsystems.get(i).toString());
@@ -15,6 +25,10 @@ public class MainMenuUI {
         System.out.println(subsystems.size() + ": Exit");
     }
 
+    /**
+     * Displays the main menu to the user.
+     * @param subsystems the subsystems that implement the actions the users can take
+     */
     public void displayMainMenu(Map<Integer, IRunnable> subsystems) {
         System.out.flush();
         System.out.println(" _________  ____  ____   ______ \n|  _   _  ||_   ||   _|.' ___  | \n|_/ | | " +
@@ -24,6 +38,10 @@ public class MainMenuUI {
         displayOptions(subsystems);
     }
 
+    /**
+     * Indicates to the user that their inpuit was invalid and prompts them to enter a new input.
+     * @param subsystems the subsystems that implement the actions the users can take
+     */
     public void displayInvalidInput(Map<Integer, IRunnable> subsystems) {
         System.out.flush();
         System.out.println("Invalid input! Try again:");
