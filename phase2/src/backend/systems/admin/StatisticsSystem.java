ff2df4e646499dfc6b9979e07ac653c4fefb2489
Christopher Matthew <chrismattthema@gmail.com>
2020-12-03 04:24:09 +0700

    Fix and finished StatisticsSystem.java

--- phase2/src/backend/systems/admin/StatisticsSystem.java
+++ phase2/src/backend/systems/admin/StatisticsSystem.java
@@ -1,7 +1,6 @@
 package backend.systems.admin;
 
 import backend.systems.MenuSystem;
-import backend.systems.admin.StatisticsCalculator;
 import backend.systems.events.managers.EventManager;
 import backend.systems.usermangement.managers.UserManager;
 import frontend.StatisticsUI;
@@ -9,17 +8,13 @@ import frontend.StatisticsUI;
 import java.time.LocalDateTime;
 
 public class StatisticsSystem extends MenuSystem {
-    private EventManager eventManager;
-    private UserManager userManager;
-    private StatisticsUI statisticsUI;
-    private StatisticsCalculator statisticsCalculator;
+    private final StatisticsUI statisticsUI;
+    private final StatisticsCalculator statisticsCalculator;
 
-    public StatisticsSystem(int numOptions, EventManager eventManager, UserManager userManager, StatisticsCalculator statisticsCalculator) {
+    public StatisticsSystem(int numOptions, EventManager eventManager, UserManager userManager) {
         super(numOptions);
-        this.eventManager = eventManager;
-        this.userManager = userManager;
         this.statisticsUI = new StatisticsUI(userManager);
-        this.statisticsCalculator = statisticsCalculator;
+        this.statisticsCalculator = new StatisticsCalculator(eventManager, userManager);
     }
 
     protected void displayOptions() { statisticsUI.displayStatisticsOptions(); }
@@ -27,30 +22,23 @@ public class StatisticsSystem extends MenuSystem {
     protected void processInput(int input) {
         switch (input) {
             case(1):
-                statisticsUI.displayEventStats(statisticsCalculator.Top5Events(), statisticsCalculator.AverageNumberOfAttendees());
+                statisticsUI.displayEventStats(statisticsCalculator.Top5Events(),
+                        statisticsCalculator.AverageNumberOfAttendees());
                 // Display top 5 most attended events, average number of people participating at events,
                 break;
             case(2):
-                statisticsUI.displayTrafficStats(statisticsCalculator.TrafficNumber(LocalDateTime.now().minusDays(1),LocalDateTime.now()),statisticsCalculator.TrafficNumber(LocalDateTime.now().minusDays(7),LocalDateTime.now()),statisticsCalculator.TrafficNumber(LocalDateTime.now().minusDays(30),LocalDateTime.now()));
+                statisticsUI.displayTrafficStats(statisticsCalculator.TrafficNumber(LocalDateTime.now().minusDays(1),
+                        LocalDateTime.now()),statisticsCalculator.TrafficNumber(LocalDateTime.now().minusDays(7),
+                        LocalDateTime.now()),statisticsCalculator.TrafficNumber(LocalDateTime.now().minusDays(30),
+                        LocalDateTime.now()));
                 // Display traffic stats (logged in past day, 7 days, month).
                 break;
             case(3):
                 statisticsUI.displaySpeakerStats(statisticsCalculator.Top5Speaker());
                 // Display the top 5 speakers who spoke at the most events
                 break;
-            case(4):
-                displayOptions();
-                break;
         }
     }
-
-    //TODO: functions here to calculate stats, pass ints to UI
-    //TODO: functions will be called from StatisticsCalculator use case class.
-    //Daily Logins
-    //Weekly Logins
-    //Monthly Logins
-    //topspeaker (5 array)
-    //topevent (5 array)
 
     @Override
     public String toString() { return "Statistics"; }
