fd7e3b9045a91b8d3501b1b4e305c6a2971cc3a2
demadani <daniel.dema@mail.utoronto.ca>
2020-12-07 23:59:37 -0500

    made a small docstring change

--- phase2/src/utility/security/Hasher.java
+++ phase2/src/utility/security/Hasher.java
@@ -3,7 +3,7 @@ package utility.security;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * A interface to which Hasher classes must implement.
+ * An interface to which Hasher classes must implement.
  * Note: Not in use right now, for phase 2.
  */
 public interface Hasher {
